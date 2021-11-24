a7f937f9c3d518516c2b2a7c6960aafb6302f401
Benni Rogge 🦕 <benni.rogge@gmail.com>
2021-11-23 18:11:09 -0800

    fix(nyt): 🐛 remove question mark from cache file

--- nyt.sh
+++ nyt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Get a random article from the past months from the New York Times.
-#
+# 
 month_in_past=$(($1-1))
 cache_dir="$HOME/.random-articles"
 mkdir -p "$cache_dir"
@@ -10,7 +10,7 @@ for i in $(seq 0 $month_in_past)
 do
   api_month=$(date -v-"${i}"m +"%-m")
   api_year=$(date -v-"${i}"m +"%Y")
-  last_modified_date=$(curl -s "https://api.nytimes.com/svc/archive/v1/${api_year}/${api_month}.json?api-key=$NYT_KEY" -I | grep "Last-Modified" | sed "s/Last-Modified: //g" | sed "s/[ ,:]/_/g")
+  last_modified_date=$(curl -s "https://api.nytimes.com/svc/archive/v1/${api_year}/${api_month}.json?api-key=$NYT_KEY" -I | grep "Last-Modified" | sed "s/Last-Modified: //g" | sed "s/[ ,:]/_/g"| tr -d '\n' | tr -d '\r') 
   cache_file="$cache_dir/$api_year.$api_month.$last_modified_date.json"
   if [ ! -f "$cache_file" ]
   then
