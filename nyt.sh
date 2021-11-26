5c27742086f99fb02237f260f5161d5eafb42f8f
Benni Rogge 🦕 <benni.rogge@gmail.com>
2021-11-26 09:34:22 -0800

    feat(nyt): ⚡️reduce calls to NYT API
    no calls for articles from previous months, if cached. Articles from current month are refreshed e

--- nyt.sh
+++ nyt.sh
@@ -6,16 +6,24 @@ month_in_past=$(($1-1))
 cache_dir="$HOME/.random-articles"
 mkdir -p "$cache_dir"
 temp_file=$(mktemp)
+
+function retrieveArticles {
+  curl -s "$1" | jq ".response.docs[].web_url" > "$2"
+}
+
 for i in $(seq 0 $month_in_past)
 do
   api_month=$(date -v-"${i}"m +"%-m")
   api_year=$(date -v-"${i}"m +"%Y")
-  last_modified_date=$(curl -s "https://api.nytimes.com/svc/archive/v1/${api_year}/${api_month}.json?api-key=$NYT_KEY" -I | grep "Last-Modified" | sed "s/Last-Modified: //g" | sed "s/[ ,:]/_/g"| tr -d '\n' | tr -d '\r') 
-  cache_file="$cache_dir/$api_year.$api_month.$last_modified_date.json"
+  cache_file="$cache_dir/$api_year.$api_month.json"
+  url="https://api.nytimes.com/svc/archive/v1/${api_year}/${api_month}.json?api-key=$NYT_KEY"
   if [ ! -f "$cache_file" ]
   then
-    url="https://api.nytimes.com/svc/archive/v1/${api_year}/${api_month}.json?api-key=$NYT_KEY"
-    curl -s "$url" | jq ".response.docs[].web_url" > "$cache_file"
+     retrieveArticles "$url" "$cache_file"
+  # if cache is for current month, we check once every twelve hours for new data
+  elif [ "$i" -eq 0 ] && [ "$(find "$cache_file" -mmin +720)" ]
+  then
+     retrieveArticles "$url" "$cache_file"
   fi
   cat "$cache_file" >> "$temp_file"
 done
